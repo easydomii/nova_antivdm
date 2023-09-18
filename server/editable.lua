bdab1d232a92a5e6d00c389e46d772f13142afcd
Domi <106663559+easydomii@users.noreply.github.com>
2023-09-18 15:03:02 +0200

    Update editable.lua

--- server/editable.lua
+++ server/editable.lua
@@ -4,16 +4,16 @@ function punishPlayer(target, case)
     local message = ""
 
     if case == "event" then
-        message = string.format("Sei stato kikkato/bannato per aver triggerato un evento (FREE ANTI-TRIGGER by https://discord.gg/XxtCBxwB)")
+        message = string.format("Sei stato kikkato/bannato, um ein Ereignis auszulösen (FREE ANTI-TRIGGER von https://discord.gg/XxtCBxwB)")
         createLog(true, target)
     elseif case == "vdm" then
-        message = string.format("Sei stato kickato/bannato per aver fatto vari vdm all'interno di questo server!")
+        message = string.format("Sie haben den Status „Kickato/Bannato“ erhalten, um sicherzustellen, dass Sie auf diesem Server viele verschiedene Funktionen haben!")
     end
 
     if message == "" then return end
 
-    local header = "Nova Scripts | ANTI VDM SYSTEM"
-    local footer = "Se credi che sia stato un errore, contatta l'assistenza!. \n by Nova Service (https://discord.gg/XxtCBxwB) \nANTIVDM SYSTEM"
+    local header = "Galaxy City | ANTI VDM SYSTEM"
+    local footer = "Wenn Sie glauben, dass Sie sich irren, wenden Sie sich an den Assistenten! \n von Nova Service (https://discord.gg/XxtCBxwB) \nANTIVDM SYSTEM"
 
     local fullMessage = string.format("%s\n\n%s\n\n%s", header, message, footer)
 
